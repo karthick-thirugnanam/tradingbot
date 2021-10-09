f8ed704a45a4cf1541506483a899a3d4847849e8
Akilesh <akilesh.v@gofrugal.com>
2021-10-09 12:23:53 +0530

    [AK] Project base is Added

---
+++ src/main/java/com/innov/bot/crypto/tradingbot/TradingbotApplication.java
@@ -0,0 +1,13 @@
+package com.innov.bot.crypto.tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innov/bot/crypto/tradingbot/TradingbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innov.bot.crypto.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
